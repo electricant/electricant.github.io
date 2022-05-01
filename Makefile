43eab7e9b57e399777cad094774f23a938fa22a6
Electric Ant <electricant@anche.no>
2022-05-01 16:26:44 +0200

    Support running the website locally

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
+# sbcl location and parameters
+SBCL=sbcl --noinform --noprint --non-interactive --no-sysinit --no-userinit
+
 all:
-	@sbcl --noinform --noprint --non-interactive --no-sysinit --no-userinit \
-		--load make-website.lisp --eval '(make-website)'
+	@$(SBCL) --load make-website.lisp --eval '(make-website)'
 
 clean:
 	@rm docs/*.html docs/articles/*html
+
+demo:
+	@$(SBCL) --load make-website.lisp --eval '(run-demo)'
+
--- make-website.lisp
+++ make-website.lisp
@@ -24,9 +24,10 @@
 
 ;;; Load required packages. Adjust quicklisp location if needed
 (load "~/.quicklisp/setup.lisp")
+(ql:quickload "hunchentoot" :silent T)
 (ql:quickload "cl-markdown" :silent T)
 (ql:quickload "local-time" :silent T)
-(ql::quickload "cl-ppcre" :silent T)
+(ql:quickload "cl-ppcre" :silent T)
 
 ;;;
 ;;; Global configuration variables
@@ -122,3 +123,13 @@
 ;;; Make the website using the functions above
 (defun make-website ()
 	(mapc #'(lambda(fl) (build-file (car fl) (cdr fl))) (sources-alist)))
+
+;;; Run a demo version of the website locally
+(defun run-demo ()
+	(let ((acceptor (hunchentoot:start (make-instance 'hunchentoot:acceptor
+	                 :port 4242 :document-root *target-dir*))))
+		(princ "Insert any input to stop the webserver...")
+		(terpri)
+		(finish-output)
+		(read)
+		(hunchentoot:stop acceptor)))
